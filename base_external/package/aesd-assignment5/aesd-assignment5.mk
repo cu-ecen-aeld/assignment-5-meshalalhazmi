7f4c16eb85c15fc0e7e05d3d08240e7292a6b85e
meshal <meshal.alhazmi@gmail.com>
2026-01-19 03:31:23 -0600

    assignment 5 part 2

---
+++ base_external/package/aesd-assignment5/aesd-assignment5.mk
@@ -0,0 +1,28 @@
+
+##############################################################
+#
+# AESD-ASSIGNMENTS
+#
+##############################################################
+
+#TODO: Fill up the contents below in order to reference your assignment 3 git contents
+AESD_ASSIGNMENT5_VERSION = a903e1b41bbf774020499c4b621e94fb2e59dc78
+# Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
+# with ssh keys and the automated build/test system.
+# Your site should start with git@github.com:
+AESD_ASSIGNMENT5_SITE = git@github.com:meshalalhazmi/ecea5305-assignment3.git
+AESD_ASSIGNMENT5_SITE_METHOD = git
+AESD_ASSIGNMENT5_GIT_SUBMODULES = YES
+
+define AESD_ASSIGNMENT5_BUILD_CMDS
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server all
+endef
+
+# TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
+define AESD_ASSIGNMENT5_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 -D $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin/aesdsocket
+	$(INSTALL) -m 0755 -D $(@D)/server/aesdsocket-start-stop $(TARGET_DIR)/etc/init.d/S99aesdsocket
+	 
+endef
+
+$(eval $(generic-package))
