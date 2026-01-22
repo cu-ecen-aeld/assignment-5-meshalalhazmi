878995f58bf64db6e9c38ef36620c1a5dd340244
meshal <meshal.alhazmi@gmail.com>
2026-01-21 18:51:00 -0600

    assignment 7 part 2

--- base_external/package/ldd/aesd-assignment5.mk
+++ base_external/package/ldd/aesd-assignment5.mk
@@ -14,7 +14,12 @@ LDD_SITE = git@github.com:cu-ecen-aeld/assignment-7-meshalalhazmi.git
 LDD_SITE_METHOD = git
 LDD_GIT_SUBMODULES = YES
 LDD_MODULE_SUBDIRS = misc-modules scull
-
+define LDD_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 $(@D)/scull/scull_load $(TARGET_DIR)/usr/bin/scull_load
+	$(INSTALL) -m 0755 $(@D)/scull/scull_unload $(TARGET_DIR)/usr/bin/scull_unload
+	$(INSTALL) -m 0755 $(@D)/misc-modules/module_load $(TARGET_DIR)/usr/bin/module_load
+	$(INSTALL) -m 0755 $(@D)/misc-modules/module_unload $(TARGET_DIR)/usr/bin/module_unload
+endef
 $(eval $(kernel-module))
 $(eval $(generic-package))
 
